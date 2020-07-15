03b55a610941cda6f40a22feb9b8a16371b1aca3
Akshay-ch-dj <akshaych203@gmail.com>
2020-07-15 18:01:38 +0530

    Re-factored tests and ingredients

--- app/series/views.py
+++ app/series/views.py
@@ -6,29 +6,28 @@ from core.models import Tag, Character
 from series import serializers
 
 
-# creating a 'Viewset' view based on generic viewset and use the ListModelMixin
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
+# Refractoring the code
+class BaseRecipeAttrViewset(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
     """
-    Manage tags in the Database
+    Base viewset for user owned series attributes
     """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
+    # queryset and serializer class are different on both
 
     # To pass the "test_tags_limited_to_user",need to filter objects-auth. user
     # override get_queryset function
     def get_queryset(self):
-        """Return Objects for current authenticated user only"""
+        """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-# To pass the tag tests 4&5, need to add CreateModelMixin[ viewsets are custom.
-# by adding mixins], then it needs to override the perform_create,to assign the
-# tag to the correct user
+    # To pass tag tests 4&5-need to add CreateModelMixin[viewsets are custom.
+    # using mixins], then it needs to override the perform_create,to assign the
+    # tag to the correct user.
     def perform_create(self, serializer):
-        """Create a new Tag"""
+        """Create a new object"""
         # The perform_create fn. can be used to hook into the "create object"
         # process, when a "create object" requested in viewset, this fun. evoke
         # and validated serializer will get passed as an argument, here it sets
@@ -36,22 +35,18 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class CharacterViewSet(viewsets.GenericViewSet,
-                       mixins.ListModelMixin,
-                       mixins.CreateModelMixin):
+# creating a 'Viewset' view based on generic viewset and use the ListModelMixin
+class TagViewSet(BaseRecipeAttrViewset):
+    """
+    Manage tags in the Database
+    """
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+
+class CharacterViewSet(BaseRecipeAttrViewset):
     """
     Manage Characters in the Database
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Character.objects.all()
     serializer_class = serializers.CharacterSerializer
-
-    def get_queryset(self):
-        """Return object for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    # Just like tags, to pass create character tests(TEST 4&5), need the mixin
-    def perform_create(self, serializer):
-        """Add a new character"""
-        serializer.save(user=self.request.user)
